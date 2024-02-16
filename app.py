3c84fb93b6cb5988b637760d81e5ab5dee001f02
Arup Sankar Roy <115450599+ArupSankarRoy@users.noreply.github.com>
2024-02-16 11:39:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.secret_key = 'xyzsdfg'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'user-system'
+app.config['MYSQL_DB'] = 'user-system' # Replace with your MySQL database name
 app.config['MYSQL_PORT'] = 3308  # Replace with your MySQL port
 
 mysql = MySQL(app)
